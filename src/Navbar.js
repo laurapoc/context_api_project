182008a2cb18e99d7c0e614be3126aa3f544f9b6
laurapoc <laura.pociuniene@gmail.com>
2021-05-18 20:06:02 +0300

    second way of adding 2 contexts into component

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,39 +9,52 @@ import Switch from "@material-ui/core/Switch";
 import SearchIcon from "@material-ui/icons/Search";
 import styles from "./styles/NavbarStyles";
 import { ThemeContext } from "./contexts/ThemeContext";
-import { LanguageContext } from "./contexts/LanguageContext";
+import { withLanguageContext } from "./contexts/LanguageContext";
+
+const content = {
+  english: {
+    search: "Search",
+    flag: "EN",
+  },
+  french: {
+    search: "Chercher",
+    flag: "FR",
+  },
+  spanish: {
+    search: "Buscar",
+    flag: "ESP",
+  },
+};
 
 class Navbar extends Component {
   static contextType = ThemeContext;
   render() {
     const { isDarkMode, toggleTheme } = this.context;
     const { classes } = this.props;
+    const { language } = this.props.languageContext;
+    const { search, flag } = content[language];
     return (
-      <LanguageContext.Consumer>
-        {(context) => (
-          <div className={classes.root}>
-            <AppBar position="static" color={isDarkMode ? "default" : "primary"}>
-              <Toolbar>
-                <IconButton className={classes.menuButton} color="inherit">
-                  <span>FR flag</span>
-                </IconButton>
-                <Typography className={classes.tile} variant="h6" color="inherit">
-                  App title {context.language}
-                </Typography>
-                <Switch onChange={toggleTheme} />
-                <div className={classes.grow} />
-                <div className={classes.search}>
-                  <div className={classes.searchIcon}>
-                    <SearchIcon />
-                  </div>
-                  <InputBase placeholder="Search..." classes={{ root: classes.inputRoot, input: classes.inputInput }} />
-                </div>
-              </Toolbar>
-            </AppBar>
-          </div>
-        )}
-      </LanguageContext.Consumer>
+      <div className={classes.root}>
+        <AppBar position="static" color={isDarkMode ? "default" : "primary"}>
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit">
+              <span>{flag}</span>
+            </IconButton>
+            <Typography className={classes.tile} variant="h6" color="inherit">
+              App title
+            </Typography>
+            <Switch onChange={toggleTheme} />
+            <div className={classes.grow} />
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase placeholder={`${search}...`} classes={{ root: classes.inputRoot, input: classes.inputInput }} />
+            </div>
+          </Toolbar>
+        </AppBar>
+      </div>
     );
   }
 }
-export default withStyles(styles)(Navbar);
+export default withLanguageContext(withStyles(styles)(Navbar));
--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -21,3 +21,10 @@ export class LanguageProvider extends Component {
     return <LanguageContext.Provider value={{ ...this.state, changeLanguage: this.changeLanguage}}>{this.props.children}</LanguageContext.Provider>;
   }
 }
+
+// create HOC which takes different component as an argument
+export const withLanguageContext = Component => props => (
+  <LanguageContext.Consumer>
+    {value => <Component languageContext={value} {...props} />}
+  </LanguageContext.Consumer>
+)
